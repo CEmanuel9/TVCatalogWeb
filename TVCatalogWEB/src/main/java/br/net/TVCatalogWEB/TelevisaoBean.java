99d9f7e3339940ae2d2aedaa210433ae0972f895
Carlos Emanuel Cordeiro Alves <alfaemanuel@gmail.com>
2022-04-27 15:08:13 -0300

    Configuração do ambiente de execução

---
+++ TVCatalogWEB/src/main/java/br/net/TVCatalogWEB/TelevisaoBean.java
@@ -0,0 +1,9 @@
+package br.net.TVCatalogWEB;
+
+import javax.faces.bean.ManagedBean;
+
+//Determina essa classe como um ManagedBean de nome televisaoBean
+@ManagedBean(name="televisaoBean")
+public class TelevisaoBean {
+	
+}
