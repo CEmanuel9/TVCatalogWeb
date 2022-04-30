e6a263b7a69dfb5576ae3514e35118c0d010fa99
Carlos Emanuel Cordeiro Alves <alfaemanuel@gmail.com>
2022-04-30 10:41:25 -0300

    Criação do JPAUtil.

---
+++ TVCatalogWEB/src/main/java/br/net/jpautil/JPAUtil.java
@@ -0,0 +1,25 @@
+package br.net.jpautil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	// Instância única do gerenciador de entidades
+	private static EntityManagerFactory factory = null;
+	
+	// Toda vez que essa classe for invocada, se não existir um...
+	// ...Entity Manager, ele deve ser criado.
+	static {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory("TVCatalogWEB");
+		}
+	}
+	
+	// Retorno do gerenciador de entidade.
+	// Este é um getter que cria e retorna um gerenciador de entidade a partir...
+	// ...da fábrica de gerenciadores de entidade recém-implementada.
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+}
